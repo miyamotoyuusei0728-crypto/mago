6e0cabbd4bbbfa8efc8863748c20049545ad0d6e
宮本悠生 <miyamotoyuusei@miyamotoyuuseinoMacBook-Pro.local>
2026-04-05 11:39:48 +0900

    update

--- src/app/page.js
+++ src/app/page.js
@@ -30,7 +30,7 @@ export default function Site() {
     window.scrollTo(0, 0);
   }
 }, []);
-function Cover({ ENJI, bgImage = "/images/introne.jpg", logoSrc = "/images/logo.svg" }) {
+function Cover({ ENJI, bgImage = "/images/introne.jpg", logoSrc = "/images/logo.PNG" }) {
 
   const [done, setDone] = useState(false);
   const [leaving, setLeaving] = useState(false); // フェード用
